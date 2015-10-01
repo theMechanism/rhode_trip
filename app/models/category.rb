fe82ae9af31d2f05f9f2428c6a431f82e76b4948
avifoxi <frosenox@gmail.com>
2015-10-01 13:02:02 -0400

    add join table, and association directives in cat + place

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
   belongs_to :category# possible self join if category has parents
+  has_and_belongs_to_many :places
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,7 @@ class Place < ActiveRecord::Base
   belongs_to :category
   belongs_to :author, polymorphic: true
   has_and_belongs_to_many :people
+  has_and_belongs_to_many :categories
   acts_as_taggable
   
   # validations to do:
