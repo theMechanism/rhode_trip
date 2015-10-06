f45cd8ee77ed1e1c2367e8f815f20e5c4e0690c1
avifoxi <frosenox@gmail.com>
2015-10-06 18:01:20 -0400

    admin categories up

--- app/controllers/admins/categories_controller.rb
+++ app/controllers/admins/categories_controller.rb
@@ -1,2 +1,9 @@
-class Admins::CategoriesController < AdminsController
+class Admins::CategoriesController < Admins::BaseController
+  include RestActionsConcern
+
+  def permitted_params
+    params.
+      require(:category).
+      permit(:name, :is_parent)
+  end
 end
--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -1,2 +1,9 @@
-class Admins::UsersController < AdminsController
+class Admins::UsersController < Admins::BaseController
+  include RestActionsConcern
+
+  def permitted_params
+    params.
+      require(:user).
+      permit(:display_name, :email, :password, :can_self_publish)
+  end
 end
