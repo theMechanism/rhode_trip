3835e006d805784b9903516b260316b69a84aea0
avifoxi <frosenox@gmail.com>
2015-10-06 12:01:33 -0400

    add include collection responders to AdminsCtrl

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,5 +1,9 @@
 class AdminsController < ApplicationController
   skip_before_action :authenticate_user!
 
+  # Redirects resources to the collection path (index action) instead
+  # of the resource path (show action) for POST/PUT/DELETE requests.
+  include Responders::CollectionResponder
+
   layout 'admins'
 end
