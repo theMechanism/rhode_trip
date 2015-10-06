9ec011e31be9631afb75605ebe73447b0f1f15a2
avifoxi <frosenox@gmail.com>
2015-10-06 13:32:04 -0400

    add full RestActionConcerns + ResourceUrlsConcerns, add templates to admins/base ctrl + views, rename views from people views

--- app/controllers/concerns/resource_urls_concern.rb
+++ app/controllers/concerns/resource_urls_concern.rb
@@ -5,4 +5,16 @@ module ResourceUrlsConcern
     url_for(controller: controller_path, action: :index)
   end
 
+  def new_resource_path
+    url_for(controller: controller_path, action: :new)
+  end
+
+  def edit_resource_path(resource)
+    url_for(controller: controller_path, action: :edit, id: resource)
+  end
+
+  def resource_path(resource)
+    url_for(controller: controller_path, action: :show, id: resource)
+  end
+
 end
--- app/controllers/concerns/rest_actions_concern.rb
+++ app/controllers/concerns/rest_actions_concern.rb
@@ -1,7 +1,7 @@
 module RestActionsConcern
   extend ActiveSupport::Concern
   include ResourceUrlsConcern
-  
+
   included do 
     respond_to :html
     responders :flash
@@ -14,6 +14,29 @@ module RestActionsConcern
   def index
     @collection = resource_class.all
     respond_with @collection
+  end
+  def new
+    @resource = resource_class.new
+    respond_with @resource
+  end
+  def create
+    @resource = resource_class.new(permitted_params)
+    @resource.save
+    respond_with @resource, location: collection_url
+  end
+  def edit
+    @resource = resource_class.find(params[:id])
+    respond_with @resource
+  end
+  def update
+    @resource = resource_class.find(params[:id])
+    @resource.update_attributes(permitted_params)
+    respond_with @resource, location: collection_url
+  end
+  def destroy
+    @resource = resource_class.find(params[:id])
+    @resource.destroy
+    respond_with @resource, location: collection_url
   end
 
 end
