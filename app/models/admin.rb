48bcfb1c374f4355e6d2634d9acc73fce69a45c6
avifoxi <frosenox@gmail.com>
2015-10-06 15:33:02 -0400

    remove assocation to people + places in admin model

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,8 +3,6 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :people, as: :author
-  has_many :places, as: :author
 
   validates :display_name, presence: true, uniqueness: true
 end
