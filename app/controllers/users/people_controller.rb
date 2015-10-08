cc72f8c3f8af6895355f49e4a8a9fffe4cca3c9c
avifoxi <frosenox@gmail.com>
2015-10-08 16:54:20 -0400

    second feature test passing -- but now first is failing

--- app/controllers/users/people_controller.rb
+++ app/controllers/users/people_controller.rb
@@ -5,7 +5,11 @@ class Users::PeopleController < UsersController
   end
 
   def create
-    @person = Person.create(person_params)
+
+    @person = Person.create( person_params) #.merge( get_tags )
+    if tags
+      @person.tags.create(name: tags)
+    end
     redirect_to users_person_path(@person)
   end
 
@@ -20,6 +24,10 @@ class Users::PeopleController < UsersController
     params.require(:person).permit(:name, :abstract, :description, :occupation, 
       :acts_as_taggable_on_tag => :name, 
       :place_ids => [] )
+  end
+
+  def tags
+    person_params[:acts_as_taggable_on_tag][:name]
   end
 
 end
--- spec/controllers/users/people_controller_spec.rb
+++ spec/controllers/users/people_controller_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Users::PeopleController, type: :controller do
 
+  describe 'create' do 
+    it 'accepts nested attributes for acts_as_taggable_on' do
+      
+    end
+  end
 end
