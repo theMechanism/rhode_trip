9d9f7f1e9f077de19ec487633e59f9f398be6e2e
avifoxi <frosenox@gmail.com>
2015-10-13 15:10:28 -0400

    add conf to Upload class

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -7,4 +7,13 @@ class Upload < ActiveRecord::Base
   has_attached_file :attached, styles: { medium: "300x300>", thumb: "100x100>" }#, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
+  has_attached_file :download,
+                    :storage => :s3,
+                    :s3_credentials => Proc.new{|a| a.instance.s3_credentials }
+
+  def s3_credentials
+    {:bucket => ENV['AWS_S3_BUCKET'], :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']}
+  end
+
+
 end
--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,1 +1,1 @@
-Figaro.require_keys('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY')
+Figaro.require_keys('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_S3_BUCKET')
