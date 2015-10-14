d92c4d531aa0e006974150f316137e43e7c0dc74
avifoxi <frosenox@gmail.com>
2015-10-14 12:33:32 -0400

    clean up initializers, remove aws

--- config/initializers/figaro.rb
+++ config/initializers/figaro.rb
@@ -1,3 +1,1 @@
 Figaro.require_keys('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_S3_BUCKET', 'AWS_REGION')
-
-# storage = Fog::Storage.new(:provider => 'AWS', :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'], :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'])
--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,4 +1,3 @@
 Paperclip::Attachment.default_options[:storage] = :fog
 Paperclip::Attachment.default_options[:fog_credentials] = { provider: "AWS", aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'], aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'] }
 Paperclip::Attachment.default_options[:fog_directory] = ENV['AWS_S3_BUCKET']
-# Paperclip::Attachment.default_options[:fog_host] = "http://localhost:3000"
