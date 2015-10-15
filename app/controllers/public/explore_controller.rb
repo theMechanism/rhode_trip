69cab591d4d83121a87244b61031c2a1ef589728
avifoxi <frosenox@gmail.com>
2015-10-15 14:39:55 -0400

    loading basic map

--- app/controllers/public/explore_controller.rb
+++ app/controllers/public/explore_controller.rb
@@ -1,6 +1,7 @@
 class Public::ExploreController < ApplicationController
   skip_before_action :authenticate_user!, :authenticate_admin!
 
+  layout false
   def index
     @people = Person.where(approved: true)
     @places = Place.where(approved: true)
